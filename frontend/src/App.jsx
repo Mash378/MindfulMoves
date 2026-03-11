b4dd93485b5f829aabca8c9385f725473c53d63b
Jaden Nelson <119365984+jadennelson729@users.noreply.github.com>
2026-03-10 22:59:43 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,24 @@ import Home from "./Home.jsx";
 import SignUp from "./SignUp.jsx";
 import Login from "./Login.jsx";
 import Game from "./Game.jsx";
-import Leaderboard from "./Leaderboard.jsx";
+import Leader_Board from "./Leader_Board.jsx";
 import Settings from "./Settings.jsx";
 import Credits from "./Credits.jsx";
+import { SettingsProvider } from "./SettingsContext.jsx"; 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/credits" element={<Credits />} />
-    </Routes>
+    <SettingsProvider> {}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/leaderboard" element={<Leader_Board />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/credits" element={<Credits />} />
+      </Routes>
+    </SettingsProvider>
   );
 }
 
